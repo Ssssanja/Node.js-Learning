debf4144a2780ba8f62ce41ec333787202813154
Ssssanja <7978005@gmail.com>
2021-06-18 20:35:38 +0300

    WebPageCreation

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const express = require('express')
 
 const app = express()
 
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
 app.get('/', (req, res) => { //первый параметр - роут (слэш), второй - функция, кот. принимает реквэст и респонс
-    res.end('Hello from nodeJS')
+    res.render('index')
 })
 
 app.listen(3000, ()=> {  //первый параметр - порт, на котором запускаем сервер, второй - колбэк
